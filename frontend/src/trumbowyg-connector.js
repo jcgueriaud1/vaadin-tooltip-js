2f549172448ba297da80d46c78f43aacf164398c
Jean-Christophe Gueriaud <jean-christophe@vaadin.com>
2020-02-20 23:27:45 +0200

    Import SVG from webpack

--- frontend/src/trumbowyg-connector.js
+++ frontend/src/trumbowyg-connector.js
@@ -1,10 +1,13 @@
 
 import 'trumbowyg/dist/trumbowyg.min.js';
+import $fonts from 'trumbowyg/dist/ui/icons.svg';
+const tpl = document.createElement('div');
+tpl.id = "trumbowyg-icons";
+tpl.innerHTML = `${$fonts}`;
+document.body.appendChild(tpl);
 
 window.initTrumbowyg = function(element) {
-        jQuery(element).trumbowyg({
-            svgPath: 'icons.svg' // or a path like '/assets/my-custom-path/icons.svg'
-        });
+        jQuery(element).trumbowyg();
 };
 
 
